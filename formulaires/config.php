df97cf74d5356a50def260e09d29bf0578d4e47c
Daniel Quisbert <danielquisbert.info@gmail.com>
2013-06-23 22:34:15 -0400

    nuevos campos para la informacion de usuario

--- formulaires/config.php
+++ formulaires/config.php
@@ -6,5 +6,16 @@ $ldapconfig['port'] = "389";
 $ldapconfig['basedn'] = "ou=usuarios,dc=geo,dc=gob,dc=bo";
 $ldapconfig['ldap_dn'] = "ou=grupos,dc=geo,dc=gob,dc=bo";
 //$ldapconfig['filter'] = "(&(cn=GS_ADMIN))";
-$ldapconfig['filter'] = "(&(cn=informaticos))";
+$ldapconfig['filter'] = "(&(cn=geografos)";
+
+//Datos de usuario
+$info['cn'] = "";
+$info['sn'] = "";
+$info['mail'] = "";
+$info['objectclass'] = "inetOrgPerson";
+$info['o'] = "GeoBolivia";
+$info['ou'] = "usuarios";
+$info['pass'] = "";
+$info['username'] = "";
+
 ?>
--- formulaires/test.php
+++ formulaires/test.php
@@ -1,52 +1,67 @@
-<?php 
+<?php
 include ("contacto_automatico_geobolivia.php");
 include ("config.php");
 ?>
 <!DOCTYPE html>
 <html>
-<head><title>Pruebsa para LDAP</title>
-	<meta content="text/html; charset=ISO-8859-15" lang="es" />
-	<style>
-		ul{
-			list-style:none;
-		}
-		label{
-			width: 120px;
-			float: left;
-		}
-		
-	</style>
-</head>
-<body>
-<form method="post">
-	<ul>
-		<li><label>usuario</label><input type=text name="usuario"></li>
-		<li><label>email</label><input type=text name="email"></li>
-		<li><label>Password</label><input type=text name="pass"></li>
-		<li><label>Telefono</label><input type=text name="email"></li>
-		<li><input type="submit" value="Verificar"</li>
-		
-		<?php
-		if($_POST['usuario']){
-			$v=verifica($ldapconfig,$_POST);
-			if($v == 1)
-				echo $_POST['usuario']." ya existe";
-			else{
-				if($v == 2)
-					echo $_POST['email']." ya existe";
-				else 
-					if($v == 3)
-						echo $_POST['usuario']." y el email ". $_POST['email']." ya existen";	
-					else
-						echo $_POST['usuario']." y el email ". $_POST['email']." NO existen";	
+	<head>
+		<title>Pruebsa para LDAP</title>
+		<meta content="text/html; charset=ISO-8859-15" lang="es" />
+		<style>
+			ul {
+				list-style: none;
 			}
-				 
-				
-		}	
-		else
-			echo"<br>No hay Valor enviado por POST";
-		?>
-	</ul>
-</form>
-</body>
+			label {
+				width: 120px;
+				float: left;
+			}
+
+		</style>
+	</head>
+	<body>
+		<form method="post">
+			<ul>
+				<li>
+					<label>usuario</label>
+					<input type=text name="usuario">
+				</li>
+				<li>
+					<label>email</label>
+					<input type=text name="email">
+				</li>
+				<li>
+					<label>Password</label>
+					<input type=text name="pass">
+				</li>
+				<li>
+					<label>Telefono</label>
+					<input type=text name="email">
+				</li>
+				<li>
+					<input type="submit" value="Verificar"</li>
+
+					<?php
+					if ($_POST['usuario']) {
+						$v = verifica($ldapconfig, $_POST);
+						if ($v == 1)
+							echo $_POST['usuario'] . " ya existe";
+						else {
+							if ($v == 2)
+								echo $_POST['email'] . " ya existe";
+							else {
+								if ($v == 3)
+									echo $_POST['usuario'] . " y el email " . $_POST['email'] . " ya existen";
+								else
+									echo $_POST['usuario'] . " y el email " . $_POST['email'] . " NO existen";
+							}
+						}
+
+					} else
+						echo "
+					<br>
+					No hay Valor enviado por POST";
+					?>
+			</ul>
+		</form>
+	</body>
 </html>
